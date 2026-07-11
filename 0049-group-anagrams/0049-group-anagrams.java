511f86f6df0670d85c39c4339409d2399d946490
Ajay Pratap Ashish <145103886+AJAY989444@users.noreply.github.com>
2026-07-11 22:42:05 +0530

    Time: 28 ms (6.36%), Space: 50.8 MB (10.77%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,14 +1,14 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] arr) {
         HashMap<String,List<String>> map = new HashMap<>();
-        
-        for(int i = 0;i < arr.length;i++) {
-            String key = AnagramKey(arr[i]);
+        for(int i = 0; i < arr.length; i++) {
+            String key = Anagram(arr[i]);
             if(!map.containsKey(key)) {
                 map.put(key,new ArrayList<>());
-            }
+            } 
             map.get(key).add(arr[i]);
         }
+
         List<List<String>> ans = new ArrayList<>();
         for(String key : map.keySet()) {
             ans.add(map.get(key));
@@ -16,16 +16,18 @@ class Solution {
         return ans;
     }
 
-    public String AnagramKey(String str) {
+    public static String Anagram(String str) {
         int[] freq = new int[26];
         for(int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             freq[ch - 'a']++;
         }
+
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < freq.length; i++) {
+        for(int i = 0 ; i < freq.length; i++) {
             sb.append(freq[i] + " ");
         }
+
         return sb.toString();
     }
 }
