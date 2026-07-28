0903e2c79be268d5cd53dfbca568e2b33660eb1f
Ajay Pratap Ashish <145103886+AJAY989444@users.noreply.github.com>
2026-07-28 15:01:15 +0530

    Time: 22 ms (45.76%), Space: 54 MB (52.65%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,31 +1,23 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length()][text2.length()];
-        for(int [] row : dp) {
-            Arrays.fill(row,-1);
-        }
-        return lcs(text1,text2, 0,0,dp);
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        return lcsBU(text1,text2,dp);
     }
 
-    public int lcs(String t1, String t2, int i ,int j,int[][] dp) {
-        int ans = 0;
+    public int lcsBU(String t1, String t2, int[][] dp) {
+        for(int i = 1; i < dp.length; i++) {
+            for(int j = 1; j < dp[0].length; j++) {
+                if(t1.charAt(i - 1) == t2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    int f = dp[i - 1][j];
+                    int s = dp[i][j - 1];
 
-        if(i==t1.length() || j == t2.length()) {
-            return 0;
+                    dp[i][j] = Math.max(f,s);
+                }
+            }
         }
 
-        if(dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        if(t1.charAt(i) == t2.charAt(j)) {
-            ans = 1 + lcs(t1,t2,i + 1, j + 1,dp);
-        } else {
-            int f = lcs(t1,t2,i+1,j,dp);
-            int s = lcs(t1,t2,i,j + 1,dp);
-            ans = Math.max(f,s);
-        }
-
-        return dp[i][j] = ans;
+        return dp[dp.length - 1][dp[0].length - 1];
     }
 }
